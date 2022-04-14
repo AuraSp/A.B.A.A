3e75e34a8eaa3feff336d17afea621e11bbf65fa
Aura Špejeraitė <ArSpej@gmail.com>
2022-04-14 19:00:35 +0300

    created backend for income

---
+++ Back/Pajamos/routes/IncomeRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const {
+  getAllIncomes,
+  createIncome,
+  getIncomeById,
+} = require("../controllers/incomesController");
+
+const router = express.Router();
+
+router.route("/").get(getAllIncomes).post(createIncome);
+
+router
+  .route("/:id")
+  .get(getIncomeById)
+
+module.exports = router;
