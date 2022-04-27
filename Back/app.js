74b34dbb818c2c01d1e48fa3023f0b067b27c675
Aura Špejeraitė <ArSpej@gmail.com>
2022-04-27 09:13:15 +0300

    crossorigin PATCH

--- Back/app.js
+++ Back/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PATCH");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
