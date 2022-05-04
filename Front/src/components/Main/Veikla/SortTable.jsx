e9427355e0330e3697cf3a5c3e2662fb3ec79c78
edgaras <edgarasulkosevic123@gmail.com>
2022-05-04 12:49:59 +0300

    date filter

--- Front/src/components/Main/Veikla/SortTable.jsx
+++ Front/src/components/Main/Veikla/SortTable.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
 import './activitiesmain.css';
 
-function SortTable() {
+
+    
+function SortTable(e) { 
+  // e.prevendDefault()
+const timeElapsed = Date.now();
+    const today = new Date(timeElapsed).toISOString().substring(0, 10);
+
+    function searchDate() {
+      var input_startDate, input_stopDate, table, tr, i;
+    
+      // get the values and convert to date
+      input_startDate = new Date(document.getElementById("date-start").value);
+      input_stopDate = new Date(document.getElementById("date-stop").value);
+    
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+    
+      for (i = 0; i < tr.length; i++) {
+        // you need to get the text and convert to date
+        let td_date = new Date(tr[i].getElementsByTagName("td")[0].textContent);
+    
+        // now you can compare dates correctly
+        if (td_date) {
+          if (td_date >= input_startDate && td_date <= input_stopDate) {
+            // show the row by setting the display property
+            tr[i].style.display = 'table-row;';
+          } else {
+            // hide the row by setting the display property
+            tr[i].style.display = 'none';
+          }
+        }
+    
+      }
+    }
+
   return (
-    <div className='activitiestable border border-1 border-muted mx-auto my-4 p-5 shadow'>SortTable</div>
-  )
-}
+  <div className='activitiestable border border-1 border-muted mx-auto my-4 p-5 shadow'>SortTable
+  <br />
+    <label className='text-start'>Pradžia</label>
+      <input id="date-start"                      
+        type='date'
+          min='1990-01-01'
+          max='2030-01-01'
+          pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+          lassName='border' ></input>
+
+     <label className='text-start'>pabaiga</label>
+      <input id="date-stop"                      
+        type='date'
+          defaultValue={today}
+          min='1990-01-01'
+          max='2030-01-01'
+          pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+          lassName='border' ></input>
+             <div><button onClick={(e)=> searchDate(e)}>filter</button></div>
+    </div>
+      )}
+
 
 export default SortTable
