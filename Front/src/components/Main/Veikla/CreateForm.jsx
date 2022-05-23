518788e3159dee4539772720beb94777f51263c0
Edgaras-L <edgaraslukosevic123@gmail.com>
2022-05-23 12:01:52 +0300

    well

--- Front/src/components/Main/Veikla/CreateForm.jsx
+++ Front/src/components/Main/Veikla/CreateForm.jsx
@@ -16,6 +16,7 @@ function CreateForm({ handlepopupClose, userId, render, setRender }) {
     const [description, setDescription] = useState("");
     const [amount, setAmount] = useState("");
     const [category, setCategory] = useState("");
+    const [load, setLoad] = useState(true)
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed).toISOString().substring(0, 10);
 
@@ -104,10 +105,12 @@ function CreateForm({ handlepopupClose, userId, render, setRender }) {
     let [categories, setCategories] = useState([]);
 
     const getAllCategories = async () => {
+        
         fetch('http://localhost:3000/api/v1/categories')
         .then(res => res.json())
         .then((json) => {
-            setCategories(json.data.categories);
+            setCategories(json.data.categories[0].category);
+            setLoad(false)
         })
     }
 
@@ -167,16 +170,16 @@ function CreateForm({ handlepopupClose, userId, render, setRender }) {
                         </div>
                     </div>
                     <label className='text-start'>Kategoriją</label>
-                    <select
+                    {!load && <select
                         {...register('category')}
                         defaultValue=''
                         onChange={(e) => setCategory(e.target.value)}
                         className='border bg-transparent text-muted'>
                         <option value='' disabled>--Pasirinkite kategoriją--</option>
                         {categories.map(item => {
-                            return (<option key={item.value} value={item.value}>{item.text}</option>)
+                            return (<option key={item.value} value={item.value}>{item.value}</option>)
                         })}
-                    </select>
+                    </select>}
                     <p className=' p-0 text-danger'>{errors.category?.message}</p>
                     <div className='formfooter d-flex flex-row flex-wrap mt-5'>
                         <div className='me-4'>
